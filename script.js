99609cb96ab1186459853427c99fe774dc0cd9bd
Abhinaba <abhinabadas91@gmail.com>
2023-11-18 23:54:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let akey=new Audio("../notes/A.mp3")
+let abkey=new Audio("../notes/Ab.mp3")
+let bkey=new Audio("../notes/B.mp3")
+let bbkey=new Audio("../notes/Bb.mp3")
+let ckey=new Audio("../notes/C.mp3")
+let dkey=new Audio("../notes/D.mp3")
+let dbkey=new Audio("../notes/Db.mp3")
+let ekey=new Audio("../notes/E.mp3")
+let ebkey=new Audio("../notes/Eb.mp3")
+let fkey=new Audio("../notes/F.mp3")
+let gkey=new Audio("../notes/G.mp3")
+let gbkey=new Audio("../notes/Gb.mp3")
+document.querySelector('[data-a]').addEventListener("click",()=>{
+    console.log("clicked a")
+    akey.play()
+})
+document.querySelector('[data-ab]').addEventListener("click",()=>{
+    console.log("clicked ab")
+    abkey.play()
+})
+document.querySelector('[data-b]').addEventListener("click",()=>{
+    console.log("clicked b")
+    bkey.play()
+})
+document.querySelector('[data-bb]').addEventListener("click",()=>{
+    console.log("clicked bb")
+    bbkey.play()
+})
+document.querySelector('[data-c]').addEventListener("click",()=>{
+    console.log("clicked c")
+    ckey.play()
+})
+document.querySelector('[data-d]').addEventListener("click",()=>{
+    console.log("clicked d")
+    dkey.play()
+})
+document.querySelector('[data-db]').addEventListener("click",()=>{
+    console.log("clicked db")
+    dbkey.play()
+})
+document.querySelector('[data-e]').addEventListener("click",()=>{
+    console.log("clicked e")
+    ekey.play()
+})
+document.querySelector('[data-eb]').addEventListener("click",()=>{
+    console.log("clicked eb")
+    ebkey.play()
+})
+document.querySelector('[data-f]').addEventListener("click",()=>{
+    console.log("clicked f")
+    fkey.play()
+})
+document.querySelector('[data-g]').addEventListener("click",()=>{
+    console.log("clicked g")
+    gkey.play()
+})
+document.querySelector('[data-gb]').addEventListener("click",()=>{
+    console.log("clicked gb")
+    gbkey.play()
+})
