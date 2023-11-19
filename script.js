7843acbdb085366101c67d18a59abeffbce28203
Abhinaba <abhinabadas91@gmail.com>
2023-11-19 12:21:31 +0530

    Finally fixed the key binding program for keys

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const keyArray=document.querySelectorAll("[data-notes]")
-const audioArray=document.querySelectorAll("[data-audio]")
+const white_Keys=["a","s","d","f","g","h","j"]
+const black_Keys=["e","r","t","y","u"]
 
-for(let i=0;i<12;i++){
-    keyArray[i].addEventListener("click",()=>{
-        audioArray[i].currentTime=0
-        audioArray[i].play()
+const whiteNote=document.querySelectorAll(".whiteKeys")
+const blackNote=document.querySelectorAll(".blackKeys")
+
+document.addEventListener("keydown",(e)=>{
+    if(e.repeat) return
+    const key=e.key
+    const whiteIndex=white_Keys.indexOf(key)
+    const blackIndex=black_Keys.indexOf(key)
+    if(whiteIndex>-1) audioPlay(whiteNote[whiteIndex])
+    if(blackIndex>-1) audioPlay(blackNote[blackIndex])
+})
+
+const noteArray=document.querySelectorAll(".keys")
+
+
+noteArray.forEach(element=>{
+    element.addEventListener("click",()=>{
+        audioPlay(element)
+    })
+})
+
+function audioPlay(key){
+    const note=document.getElementById(key.dataset.notes)
+    note.currentTime=0
+    note.play()
+    key.classList.add("active")
+    note.addEventListener("ended",()=>{
+        key.classList.remove("active")
     })
 }
