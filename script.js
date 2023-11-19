eca2bfc0cc3b52e060fb06745f026728507ce755
Abhinaba <abhinabadas91@gmail.com>
2023-11-19 11:18:22 +0530

    Cleared Code logic made code better using audiotag

--- script.js
+++ script.js
@@ -1,60 +1,9 @@
-let akey=new Audio("A.mp3")
-let abkey=new Audio("Ab.mp3")
-let bkey=new Audio("B.mp3")
-let bbkey=new Audio("Bb.mp3")
-let ckey=new Audio("C.mp3")
-let dkey=new Audio("D.mp3")
-let dbkey=new Audio("Db.mp3")
-let ekey=new Audio("E.mp3")
-let ebkey=new Audio("Eb.mp3")
-let fkey=new Audio("F.mp3")
-let gkey=new Audio("G.mp3")
-let gbkey=new Audio("Gb.mp3")
-document.querySelector('[data-a]').addEventListener("click",()=>{
-    console.log("clicked a")
-    akey.play()
-})
-document.querySelector('[data-ab]').addEventListener("click",()=>{
-    console.log("clicked ab")
-    abkey.play()
-})
-document.querySelector('[data-b]').addEventListener("click",()=>{
-    console.log("clicked b")
-    bkey.play()
-})
-document.querySelector('[data-bb]').addEventListener("click",()=>{
-    console.log("clicked bb")
-    bbkey.play()
-})
-document.querySelector('[data-c]').addEventListener("click",()=>{
-    console.log("clicked c")
-    ckey.play()
-})
-document.querySelector('[data-d]').addEventListener("click",()=>{
-    console.log("clicked d")
-    dkey.play()
-})
-document.querySelector('[data-db]').addEventListener("click",()=>{
-    console.log("clicked db")
-    dbkey.play()
-})
-document.querySelector('[data-e]').addEventListener("click",()=>{
-    console.log("clicked e")
-    ekey.play()
-})
-document.querySelector('[data-eb]').addEventListener("click",()=>{
-    console.log("clicked eb")
-    ebkey.play()
-})
-document.querySelector('[data-f]').addEventListener("click",()=>{
-    console.log("clicked f")
-    fkey.play()
-})
-document.querySelector('[data-g]').addEventListener("click",()=>{
-    console.log("clicked g")
-    gkey.play()
-})
-document.querySelector('[data-gb]').addEventListener("click",()=>{
-    console.log("clicked gb")
-    gbkey.play()
-})
+const keyArray=document.querySelectorAll("[data-notes]")
+const audioArray=document.querySelectorAll("[data-audio]")
+
+for(let i=0;i<12;i++){
+    keyArray[i].addEventListener("click",()=>{
+        audioArray[i].currentTime=0
+        audioArray[i].play()
+    })
+}
